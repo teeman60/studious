15c12b96a42b4d0632a4a20a53df3fbf19ba8b6b
Taiye Salami <taiye41@gmail.com>
2021-11-11 14:53:45 -0600

    Your awesome message

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -24,14 +24,7 @@ class UsersController < ApplicationController #Api::V1:: before userscontroller
     def show 
         user = User.find_by(id: params[:id])
         render json: UserSerializer.new(user)
-        byebug
-        
-    end
-
-    def show 
-        user = User.find_by(id: params[:id])
-        render json: UserSerializer.new(user)
-        byebug
+        # byebug
         
     end
 
